9d907eb79eb862f09f43f5460190e11103c5ba78
pino._.hoo <inhoo23@naver.com>
2023-01-27 02:40:52 +0900

    Add : Todo API

--- src/api/todo/index.js
+++ src/api/todo/index.js
@@ -8,3 +8,27 @@ const todo = axios.create({
     baseURL: `${BASE_URL}/todo`,
     withCredentials: true,
 })
+
+export const saveTodoApi = async (text) => {
+    try {
+        return await todo.post('/', { text })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const updateTodoApi = async (id, text) => {
+    try {
+        return await todo.put(`/${id}`, { text })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const deleteTodoApi = async (id) => {
+    try {
+        return await todo.delete(`/${id}`)
+    } catch (err) {
+        console.log(err)
+    }
+}
