fd9f7db687a59ba48fc9b546ce3125776b748adc
pino._.hoo <inhoo23@naver.com>
2023-01-27 01:38:10 +0900

    Add : 임시 로그인 구현

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,11 +1,50 @@
 // ** React Imports
-import React from 'react'
+import React, { useState } from 'react'
+
+// ** Router Imports
+import { Link } from 'react-router-dom'
+
+// ** Api Imports
+import { login } from '../../api/auth'
 
 // ** Style
 import S from './style'
 
 function LoginPage() {
-    return <S.Text>Login Page</S.Text>
+    const [user, setUser] = useState({ id: '', password: '' })
+
+    const onChange = (e) => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value,
+        })
+    }
+
+    const onCLickLoginBtn = async () => {
+        try {
+            const res = await login(user)
+            console.log(res)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <S.Outer>
+            <S.Input placeholder="Enter ID" onChange={onChange} name="id" />
+            <S.Input
+                placeholder="Enter Password"
+                onChange={onChange}
+                type="password"
+                name="password"
+            />
+            <S.LoginBtn onClick={onCLickLoginBtn}>LOGIN</S.LoginBtn>
+            <Link to="/register">
+                <S.PText>회원가입하기</S.PText>
+            </Link>
+        </S.Outer>
+    )
 }
 
 export default LoginPage
--- src/page/login/style.js
+++ src/page/login/style.js
@@ -2,7 +2,11 @@
 import styled from 'styled-components'
 
 const S = {
+    Outer: styled.div``,
     Text: styled.h1``,
+    Input: styled.input``,
+    LoginBtn: styled.button``,
+    PText: styled.p``,
 }
 
 export default S
