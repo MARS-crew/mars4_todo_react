d201535f8291c00f10dbb4d752a2d379d4c6379d
pino._.hoo <inhoo23@naver.com>
2023-01-27 02:40:36 +0900

    Add : Todo Store 구성

--- src/store/apps/todo/index.js
+++ src/store/apps/todo/index.js
@@ -6,14 +6,68 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { BASE_URL } from '../../../api'
 
-export const fetchData = createAsyncThunk('appContents/fetchData', async (params) => {})
+const todo = axios.create({
+    baseURL: `${BASE_URL}/todo`,
+    withCredentials: true,
+})
+
+export const fetchData = createAsyncThunk('appContents/fetchData', async () => {
+    const res = await todo.get('/')
+
+    return res.data
+})
+
+export const fetchNoData = createAsyncThunk('appContents/fetchNoData', async (id) => {
+    const res = await todo.get(`/${id}`)
+
+    return res.data
+})
 
 export const appContentsSlice = createSlice({
     name: 'appContents',
-    initialState: {},
-
+    initialState: {
+        errCd: '',
+        errMsg: '',
+        responseData: [],
+        responseNoData: {},
+        loadingStat: false,
+    },
     reducers: {},
-    extraReducers: (builder) => {},
+    extraReducers: (builder) => {
+        // ** Todo Data 가져오기 peding
+        builder.addCase(fetchData.pending, (state, action) => {
+            if (!state.loadingStat) {
+                state.loadingStat = true
+            }
+        })
+
+        // ** Todo Data 가져오기fulfilled
+        builder.addCase(fetchData.fulfilled, (state, action) => {
+            state.errCd = action.payload.status
+            state.errMsg = action.payload.message
+            state.responseData = action.payload.result ? action.payload.result : null
+            if (state.loadingStat) {
+                state.loadingStat = false
+            }
+        })
+
+        // ** Todo Data 하나 가져오기 peding
+        builder.addCase(fetchNoData.pending, (state, action) => {
+            if (!state.loadingStat) {
+                state.loadingStat = true
+            }
+        })
+
+        // ** Todo Data 하나 가져오기fulfilled
+        builder.addCase(fetchNoData.fulfilled, (state, action) => {
+            state.errCd = action.payload.status
+            state.errMsg = action.payload.message
+            state.responseData = action.payload.result ? action.payload.result : null
+            if (state.loadingStat) {
+                state.loadingStat = false
+            }
+        })
+    },
 })
 
 export default appContentsSlice.reducer
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-// ** Redux imports
+// ** Redux Imports
 import { configureStore } from '@reduxjs/toolkit'
 
+// ** Reducer Imports
+import todo from './apps/todo'
+
 export const store = configureStore({
-    reducer: {},
+    reducer: { todo },
 })
