34a37910efb8180635c64226baac8231449e7a08
pinomaker-hoo <inhoo23@naver.com>
2023-01-30 02:50:35 +0900

    Add : Card Grid

--- src/views/main/thirdView.jsx
+++ src/views/main/thirdView.jsx
@@ -1,5 +1,72 @@
+// ** Mui Imports
+import { Card, Grid } from '@mui/material'
+
 function ThirdView() {
-    return <h1>Third View</h1>
+    return (
+        <Grid container spacing={6} sx={{ py: 10, px: 20 }}>
+            <Grid item xs={4}>
+                <Grid container spacing={6}>
+                    <Grid item xs={12}>
+                        <Card sx={{ height: 300 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                                <Card sx={{ height: 70 }}>Helloo</Card>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Card sx={{ height: 70 }}>Helloo</Card>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={4}>
+                <Grid container spacing={6}>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card sx={{ height: 300 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={4}>
+                <Grid container spacing={6}>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 300 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 300 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card sx={{ height: 150 }}>Helloo</Card>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default ThirdView
