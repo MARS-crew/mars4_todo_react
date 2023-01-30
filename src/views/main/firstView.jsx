2bfd751531c2bbdeec9b83b101204c86027489fe
pinomaker-hoo <inhoo23@naver.com>
2023-01-31 00:24:36 +0900

    Add : Mui Griid Item

--- src/views/main/firstView.jsx
+++ src/views/main/firstView.jsx
@@ -9,14 +9,15 @@ const S = {
     subTitle: styled.h3``,
 }
 
-
 function FirstView() {
     return (
         <Grid container spacing={6} sx={{ py: 10, px: 20 }}>
-            <S.title>저는 ‘술 그만 먹고 다른 것좀 해볼까?’ 라고 생각하는 사람입니다.</S.title>
-            <br/>
-            <S.subTitle>내가 선택한 버킷리스트는?</S.subTitle>
-            
+            <Grid item xs={12}>
+                <S.title>저는 ‘술 그만 먹고 다른 것좀 해볼까?’ 라고 생각하는 사람입니다.</S.title>
+            </Grid>
+            <Grid item xs={12}>
+                <S.subTitle>내가 선택한 버킷리스트는?</S.subTitle>
+            </Grid>
         </Grid>
     )
 }
