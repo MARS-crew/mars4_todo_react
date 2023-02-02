239595db39f9589e3d512087d0843505fc60a3d8
pinomaker-hoo <inhoo23@naver.com>
2023-02-02 17:33:56 +0900

    Update : Base URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,4 @@
 /* eslint-disable */
-export const BASE_URL = 'http://mars.pinodev.shop:8080/api'
+export const BASE_URL_DEV = 'http://mars.pinodev.shop:8080/api'
+export const BASE_URL_TEST = ''
+export const BASE_URL_LIVE = ''
