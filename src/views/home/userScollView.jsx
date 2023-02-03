f024990cf573a5b579095b23f4b9a67982a2f55a
pinomaker <inhoo23@naver.com>
2023-02-03 18:58:54 +0900

    Fix : Slider : Not Finish

--- src/views/home/userScollView.jsx
+++ src/views/home/userScollView.jsx
@@ -1,34 +1,59 @@
-// ** Scroll library
-import Slider from 'react-slick'
+// // ** Scroll library
+// import Slider from 'react-slick'
 
-// ** Styled-components Imports
-import styled from 'styled-components'
+// // ** Styled-components Imports
+// import styled from 'styled-components'
 
-// ** Other Views Imports
+// // ** Other Views Imports
 import LoginPage from '../../page/login'
 import RegisterPage from '../../page/register'
 
-const S = {
-    sliderItem: styled.div``,
-}
+// const S = {
+//     sliderItem: styled.div``,
+// }
+
+// function UserScrollView() {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//     }
+
+//     return (
+//         <Slider {...settings}>
+//             <S.sliderItem>
+//                 <LoginPage />
+//             </S.sliderItem>
+//             <S.sliderItem>
+//                 <RegisterPage />
+//             </S.sliderItem>
+//         </Slider>
+//     )
+// }
+
+// export default UserScrollView
+
+import Carousel from 'react-material-ui-carousel'
+import { Button } from '@mui/material'
 
 function UserScrollView() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-    }
-
     return (
-        <Slider {...settings}>
-            <S.sliderItem>
-                <LoginPage />
-            </S.sliderItem>
-            <S.sliderItem>
-                <RegisterPage />
-            </S.sliderItem>
-        </Slider>
+        <Carousel
+            NavButton={({ onClick, className, style, next, prev }) => {
+                // Other logic
+
+                return (
+                    <Button onClick={onClick} className={className} style={style}>
+                        {next && 'Next'}
+                        {prev && 'Previous'}
+                    </Button>
+                )
+            }}
+        >
+            <LoginPage />
+            <RegisterPage />
+        </Carousel>
     )
 }
 
