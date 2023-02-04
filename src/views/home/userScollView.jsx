391fc62de33a9c663da9784f913fba02ad0d376f
pinomaker-hoo <inhoo23@naver.com>
2023-02-04 16:13:10 +0900

    Fix : Slider

--- src/views/home/userScollView.jsx
+++ src/views/home/userScollView.jsx
@@ -1,56 +1,12 @@
-// // ** Scroll library
-// import Slider from 'react-slick'
-
-// // ** Styled-components Imports
-// import styled from 'styled-components'
-
 // // ** Other Views Imports
 import LoginPage from '../../page/login'
 import RegisterPage from '../../page/register'
 
-// const S = {
-//     sliderItem: styled.div``,
-// }
-
-// function UserScrollView() {
-//     const settings = {
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 1,
-//     }
-
-//     return (
-//         <Slider {...settings}>
-//             <S.sliderItem>
-//                 <LoginPage />
-//             </S.sliderItem>
-//             <S.sliderItem>
-//                 <RegisterPage />
-//             </S.sliderItem>
-//         </Slider>
-//     )
-// }
-
-// export default UserScrollView
-
 import Carousel from 'react-material-ui-carousel'
-import { Button } from '@mui/material'
 
 function UserScrollView() {
     return (
-        <Carousel
-            NavButton={({ onClick, className, style, next, prev }) => {
-                // Other logic
-
-                return (
-                    <Button onClick={onClick} className={className} style={style}>
-                        {next && 'Next'}
-                        {prev && 'Previous'}
-                    </Button>
-                )
-            }}
-        >
+        <Carousel>
             <LoginPage />
             <RegisterPage />
         </Carousel>
