08591637ec64183bb19bc81523647da8b4a18f4d
pinomaker-hoo <inhoo23@naver.com>
2023-02-09 13:15:54 +0900

    Add : Store 추가 - 유저, 투둗

--- src/store/apps/bucket/index.js
+++ src/store/apps/bucket/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 // ** Redux imports
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
@@ -7,13 +6,18 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { BASE_URL_DEV } from '../../../api'
 
-const auth = axios.create({
-    baseURL: `${BASE_URL_DEV}/user`,
+const todo = axios.create({
+    baseURL: `${BASE_URL_DEV}/bucket`,
     withCredentials: true,
 })
 
-export const fetchData = createAsyncThunk('appContents/fetchData', async (params) => {
-    const res = await auth.post('/login', params)
+export const fetchData = createAsyncThunk('appContents/fetchData', async () => {
+    const res = await todo.get('/')
+    return res.data
+})
+
+export const fetchNoData = createAsyncThunk('appContents/fetchNoData', async (id) => {
+    const res = await todo.get(`/${id}`)
     return res.data
 })
 
@@ -37,6 +41,23 @@ export const appContentsSlice = createSlice({
 
         // ** Todo Data 가져오기fulfilled
         builder.addCase(fetchData.fulfilled, (state, action) => {
+            state.errCd = action.payload.status
+            state.errMsg = action.payload.message
+            state.responseData = action.payload.result ? action.payload.result : null
+            if (state.loadingStat) {
+                state.loadingStat = false
+            }
+        })
+
+        // ** Todo Data 하나 가져오기 peding
+        builder.addCase(fetchNoData.pending, (state, action) => {
+            if (!state.loadingStat) {
+                state.loadingStat = true
+            }
+        })
+
+        // ** Todo Data 하나 가져오기fulfilled
+        builder.addCase(fetchNoData.fulfilled, (state, action) => {
             state.errCd = action.payload.status
             state.errMsg = action.payload.message
             state.responseData = action.payload.result ? action.payload.result : null
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,9 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // ** Reducer Imports
 import todo from './apps/todo'
+import bucket from './apps/bucket'
+import user from './apps/user'
 
 export const store = configureStore({
-    reducer: { todo },
+    reducer: { todo, bucket, user },
 })
